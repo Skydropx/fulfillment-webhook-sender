64c30c629cb97da861bcae0b4e6e563d47741764
Juan Manuel MAffei <juanmamaffei@gmail.com>
2023-01-14 18:23:57 -0300

    fix: model enum fixed

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,5 +11,5 @@
 #
 class Event < ApplicationRecord
   # Enum for mapping each topic on KafkaConfig.topics with an integer. 
-  enum topic: KafkaConfig.topics
+  enum topic: KafkaConfig.topics.each_with_index.map { |topic, index| [topic, index] }.to_h
 end
