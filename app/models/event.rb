4a5b2579c286de6ab94c79773f6eba0abfbd8f00
Juan Manuel MAffei <juanmamaffei@gmail.com>
2023-01-14 22:54:27 -0300

    feat: events triggers actions

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,6 +15,17 @@
 class Event < ApplicationRecord
   # Enum for mapping each topic on KafkaConfig.topics with an integer.
   enum topic: KafkaConfig.topics.each_with_index.map { |topic, index| [topic, index] }.to_h
+
   has_many :messages
   has_many :webhooks, through: :messages
+
+  # After an event is created, is necessary to create a message for each webhook
+  after_create :create_messages
+
+  def create_messages
+    Webhook.where(topic: topic, external_user_id: external_user_id).each do |webhook|
+      Message.create!(event_id: self.id, webhook_id: webhook.id, attempts: 0)
+    end
+  end
+
 end
